b3ae8ce64fc8e5fb94b7fd132b82d8990ed56a71
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-05-18 21:42:48 -0400

    f_resid to 0.5%

--- fig5/plots.R
+++ fig5/plots.R
@@ -14,10 +14,10 @@ tiff('../fig_output/Fig5.tiff', width = 6, height = 3.2, pointsize = 9, units = 
   lines(out6$time,out6$methanogenbiomass/1000, type = 'l', col = c('palegreen4'), lty = 2, ylab ='',xaxt='n')
   legend('topleft', legend=c(expression(italic('f'['resid'])~'= 0.5,'~italic(a['enrich'])~'= 5'),  # 1
                          expression(italic('f'['resid'])~'= 0.1,'~italic(a['enrich'])~'= 5'),  # 2
-                         expression(italic('f'['resid'])~'= 0.001,'~italic(a['enrich'])~'= 5'), # 3 
+                         expression(italic('f'['resid'])~'= 0.005,'~italic(a['enrich'])~'= 5'), # 3 
                          expression(italic('f'['resid'])~'= 0.5,'~italic(a['enrich'])~'= 0'),  # 4 
                          expression(italic('f'['resid'])~'= 0.1,'~italic(a['enrich'])~'= 0'),  # 5 
-                         expression(italic('f'['resid'])~'= 0.001,'~italic(a['enrich'])~'= 0')),# 6 
+                         expression(italic('f'['resid'])~'= 0.005,'~italic(a['enrich'])~'= 0')),# 6 
          col=c("purple","palegreen4","orange","purple","palegreen4","orange"), lty=c(1,1,1,2,2,2), 
          cex=0.6, box.lty=0, bg = 'transparent')
   mtext('a', 3, line = 1)
